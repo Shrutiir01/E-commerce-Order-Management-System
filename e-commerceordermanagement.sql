b3ff2831aea159f63cdda33c5cd40109929c6793
Shrutiir01 <shrutiraut1152@gmail.com>
2025-08-08 16:28:06 +0530

    e-commerceordermanagement.sql

--- e-commerceordermanagement.sql
+++ e-commerceordermanagement.sql
@@ -31,9 +31,9 @@ CREATE TABLE carte
 
 ----Orders
 CREATE TABLE orderse
-(order_id NUMBER PRIMARY KEY
-,customer_id NUMBER
-,order_date DATE
+(order_id     NUMBER PRIMARY KEY
+,customer_id  NUMBER
+,order_date   DATE
 ,total_amount NUMBER
 ,CONSTRAINTS orderse_customer_id_fk FOREIGN KEY(customer_id)
                                     REFERENCES customerse(customer_id)
@@ -41,11 +41,12 @@ CREATE TABLE orderse
 
 ----Order_items
 CREATE TABLE order_itemse
-(item_id NUMBER PRIMARY KEY
-,order_id NUMBER
+(item_id    NUMBER PRIMARY KEY
+,order_id   NUMBER
 ,product_id NUMBER
-,quantity NUMBER
-,price NUMBER
+,quantity   NUMBER
+,price      NUMBER
 ,CONSTRAINTS order_itemse_order_id_fk FOREIGN KEY(order_id)
                                       REFERENCES orderse(order_id)                  
 );
+
